c920b98ed4c05cedd8d1a155a9d9da19b1839e73
AleksandrAlexashin <sashokmetall@mail.ru>
2022-01-27 18:28:53 +0400

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку с разными скобками");
+            string s = Console.ReadLine();
+            //Console.WriteLine(Chek(s));
+            Chek(s);
+            Console.ReadKey();
+        }
+        static void Chek(string s)
+            
+    {
+          Stack<char> stk = new Stack<char>();
+            Dictionary<char, char> bracket = new Dictionary<char, char> 
+            { { '(', ')' }, { '[', ']' }, { '{', '}' } };
+
+
+            foreach (char b in s)
+            {
+                if (b == '(' || b == '[' || b == '{')
+                    stk.Push(bracket[b]);
+                if ((b == ')' || b == ']' || b == '}'))
+                {
+                    if (stk.Count == 0 || stk.Pop() != b)
+                    { 
+                        //return false;
+                        Console.WriteLine("Ошибка");
+
+                    }
+                }
+
+            
+            }
+
+
+                if (stk.Count == 0)
+
+                    //return true; 
+                    Console.WriteLine("Всё верно");
+                else
+                    //return false; 
+             Console.WriteLine("Ошибка");
+            
+        }        
+        
+        
+    }
+}
